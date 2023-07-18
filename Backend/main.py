e020588dccf28feac836141cdb1db28506b92134
Qin Gu <guqin7@gmail.com>
2023-07-18 16:13:38 +0200

    fix

--- Backend/main.py
+++ Backend/main.py
@@ -16,21 +16,23 @@ def process_text():
 
     tags = set()
     highlighted_text = text
-
     if "NE" in request.json:
+        highlighted_text = text
         for ent in doc.ents:
             tags.add(ent.label_)
             html_entity = f"<{ent.label_}>{html.escape(ent.text)}</{ent.label_}>"
             highlighted_text = highlighted_text.replace(ent.text, html_entity)
 
     if "POS" in request.json:
+        highlighted_text = ""
         for token in doc:
             tags.add(token.pos_)
             if token.pos_:
-                html_token = f"<{token.pos_}>{html.escape(token.text)}</{token.pos_}>"
-                highlighted_text = highlighted_text.replace(token.text, html_token)
+                html_token = f"<{token.pos_}>{token.text}</{token.pos_}>"
+                highlighted_text += html_token + " "
+            else:
+                highlighted_text += token.text + " "
 
-    print(highlighted_text)
     return jsonify({"tags": list(tags), "highlightedText": highlighted_text})
 
 
